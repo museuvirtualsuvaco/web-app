008fa22ff0804a16f312e5a6fb89d9afc07960d3
artur_amaral <artur.amaral@mettainnovations.com.br>
2025-05-20 23:28:12 -0300

    Refatora Header para CSS modules

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,24 +3,26 @@ import styles from './Footer.module.css'
 
 export default function Footer() {
 
-    return <footer className={styles.mainContainer}>
-        
-        <div className={styles.leftContainer}>
-            <p className={styles.title}>Suvaco do Cristo</p>
-            <p>Todos os direitos reservados.</p>
-        </div>
-        <div className={styles.links}>
+    return (
+        <footer className={styles.mainContainer}>
+            
+            <div className={styles.leftContainer}>
+                <p className={styles.title}>Suvaco do Cristo</p>
+                <p>Todos os direitos reservados.</p>
+            </div>
+            <div className={styles.links}>
 
-            <a href="https://www.instagram.com/suvacodocristooficial" target="_blank" rel="noopener noreferrer">
-                <i class="ri-instagram-line"></i><span> Instagram</span>
-            </a>
-            <a href="https://www.youtube.com/@blocosuvacodecristorj" target="_blank" rel="noopener noreferrer">
-                <i class="ri-youtube-line"></i><span> Youtube</span>
-            </a>
-            <a href="https://www.facebook.com/suvaco.docristo" target="_blank" rel="noopener noreferrer">
-                <i class="ri-facebook-box-line"></i><span> Facebook</span>
-            </a>
-        </div>
+                <a href="https://www.instagram.com/suvacodocristooficial" target="_blank" rel="noopener noreferrer">
+                    <i class="ri-instagram-line"></i><span> Instagram</span>
+                </a>
+                <a href="https://www.youtube.com/@blocosuvacodecristorj" target="_blank" rel="noopener noreferrer">
+                    <i class="ri-youtube-line"></i><span> Youtube</span>
+                </a>
+                <a href="https://www.facebook.com/suvaco.docristo" target="_blank" rel="noopener noreferrer">
+                    <i class="ri-facebook-box-line"></i><span> Facebook</span>
+                </a>
+            </div>
 
-    </footer>
+        </footer>
+    );
 }
