4109b8c5abb046899b9f96f1f251978128be7336
Julia Vilela <juliavsb@dcc.ufrj.br>
2025-06-29 21:52:00 -0300

    iniciei a responsividade do nav criando um menu hamburguer

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,30 +1,48 @@
+// Nav.jsx (REVISADO)
 import React from "react";
-import styles from './Nav.module.css'
+import styles from './Nav.module.css';
 import { Link } from 'react-router-dom';
+import MenuHamburguer from './MenuHamburguer/MenuHamburguer'; // Caminho relativo ajustado
 
 import * as ROUTES from '../../constants/routes';
 
-export default function Nav(){
+export default function Nav() {
     return (
         <nav className={styles.mainContainer}>
-            <ul>
-                <img className={styles.navIcon} src={null} alt="icone aqui" />
-                <div className={styles.liItens}>
-                    
-                    <li>
-                        <Link to={ROUTES.LANDING}>Início</Link>
-                    </li>
-                    <li>
-                        <Link to={ROUTES.DIVINAS_AXILAS}>Projeto Social</Link>
-                    </li>
-                    <li>
-                        <Link to={ROUTES.PROJETO_MUSEU}>Projeto Museu</Link>
-                    </li>
-                    <li>
-                        <Link to={ROUTES.SOBRE}>Sobre</Link>
-                    </li>
-                </div>
+            <img className={styles.navIcon} src={null} alt="icone aqui" />
+
+            {/* Menu Desktop */}
+            <ul className={styles.liItens}>
+                <li>
+                    <Link to={ROUTES.LANDING}>Início</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.DIVINAS_AXILAS}>Projeto Social</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.PROJETO_MUSEU}>Projeto Museu</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.SOBRE}>Sobre</Link>
+                </li>
             </ul>
+
+            {/* Menu Hambúrguer (Mobile) */}
+            {/* Passe SOMENTE os <li> para o MenuHamburguer */}
+            <MenuHamburguer>
+                <li>
+                    <Link to={ROUTES.LANDING}>Início</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.DIVINAS_AXILAS}>Projeto Social</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.PROJETO_MUSEU}>Projeto Museu</Link>
+                </li>
+                <li>
+                    <Link to={ROUTES.SOBRE}>Sobre</Link>
+                </li>
+            </MenuHamburguer>
         </nav>
     );
 }
