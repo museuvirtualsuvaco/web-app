c5a972edf5c163ac961f0b0500a32d9c2e3b420c
carloschgas <139183141+carloschgas@users.noreply.github.com>
2025-08-04 16:32:19 -0300

    interprete

--- src/components/SubConteudo/SambaConteudo.jsx
+++ src/components/SubConteudo/SambaConteudo.jsx
@@ -44,10 +44,21 @@ export default function SambaContent() {
       {/* BLOCO: Música */}
       <h2 className={styles.sectionTitle}>ESCUTE A MÚSICA</h2>
       <section className={styles.card}>
-        <audio controls className={styles.audio}>
-          <source src={musica} type="audio/mpeg" />
-          Seu navegador não suporta o elemento de áudio.
-        </audio>
+
+        <div className={styles.flexSong}>
+          <audio controls className={styles.audio}>
+            <source src={musica} type="audio/mpeg" />
+            Seu navegador não suporta o elemento de áudio.
+          </audio>
+
+          <div className={styles.profile}>
+            <span className={styles.interprete}>INTÉRPRETE</span>
+            <img className={styles.avatar} src="/src/assets/artistas/nenhum.png" alt="" />
+            <span className={styles.nome}>Beto Brown</span>
+          </div>
+
+        </div>
+
       </section>
 
       {/* BLOCO: Autores */}
@@ -56,7 +67,7 @@ export default function SambaContent() {
         <div className={styles.profileRow}>
           {Object.entries(Autores["1986"]).map(([key, autor]) => (
             <div key={key} className={styles.profile}>
-                { <img className={styles.avatar} src={autor.imgPath} alt={autor.name} />}
+              {<img className={styles.avatar} src={autor.imgPath} alt={autor.name} />}
               <span className={styles.nome}>{autor.name}</span>
             </div>
           ))}
@@ -69,9 +80,9 @@ export default function SambaContent() {
         <div className={styles.profileRow}>
           {Object.entries(Musicos["1986"]).map(([key, musico]) => (
             <div key={key} className={styles.profile}>
-              <div className={styles.avatar}>
-                {/* <img src={musico.imgPath} alt={musico.name} /> */}
-              </div>
+
+              {<img className={styles.avatar} src={musico.imgPath} alt={musico.name} />}
+
               <span className={styles.nome}>{musico.name}</span>
               <span className={styles.funcao}>{musico.funcao}</span>
             </div>
