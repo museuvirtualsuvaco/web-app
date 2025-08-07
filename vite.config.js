22ce6980882c667557c5015379cf57276a5693c5
Vitor Motta <vitor.motta@netlex.com.br>
2025-08-07 20:32:17 -0300

    Configuração pipeline

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs';
-
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 🔹 garante que assets usem caminhos relativos
   server: {
-  //   https: {
-  //     key: fs.readFileSync('./localhost-key.pem'),
-  //     cert: fs.readFileSync('./localhost.pem'),
-  //   },
+    // https: {
+    //   key: fs.readFileSync('./localhost-key.pem'),
+    //   cert: fs.readFileSync('./localhost.pem'),
+    // },
     host: true,
     port: 4567,
   }
