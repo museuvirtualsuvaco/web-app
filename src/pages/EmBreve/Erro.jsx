a6ff77fcda7a1f2d328df27889fd64c031889e5b
carloschgas <139183141+carloschgas@users.noreply.github.com>
2025-09-09 20:32:55 -0300

    Em construcao e rotas

---
+++ src/pages/EmBreve/Erro.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import styles from "./Emconstrucao.module.css";
+
+export default function Erro(){
+    return(
+    <>
+
+    <main>
+        <h1 className={styles.title}>Ops! Erro 404</h1>
+        <p className={styles.texto}></p>
+        <p className={styles.texto}>Parece que você tentou entrar em uma página que não existe</p>
+        <p className={styles.texto}>Volte para a página principal <span className={styles.link}><a href="/">aqui</a></span></p>
+        
+    </main>
+    </>)
+}
