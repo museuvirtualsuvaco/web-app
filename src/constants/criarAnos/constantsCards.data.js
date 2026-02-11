af4a957db3c89ffd5d1ef3b9875bc89d25a9846a
Carlos Chagas <139183141+carloschgas@users.noreply.github.com>
2026-02-11 19:01:33 -0300

    Update constantsCards.data.js

--- src/constants/criarAnos/constantsCards.data.js
+++ src/constants/criarAnos/constantsCards.data.js
@@ -127,7 +127,7 @@ export const constantsCardsData = [
             {
                 title: 'Samba',
                 description: "O samba-enredo retrata a essência do Carnaval como uma celebração coletiva e participativa, descrevendo a chegada da bateria como um grupo de foliões que se integram à festa, com a letra evocando os sons dos instrumentos e o espírito...",
-                src: null,
+                src: "ventarolas.jpeg",
                 firstColor: "#00BE1D",
                 secondColor: "#00DE21",
                 link: '/2012/samba'
@@ -136,7 +136,7 @@ export const constantsCardsData = [
             {
                 title: 'Camiseta',
                 description: "A Camiseta de 2012, criada por Edgar Fonseca, contém uma representação da vista aérea do bloco desfilando na rua Jardim Botânico. Na frente, a multidão sem rostos distinguíveis se aglomera em meio a guarda-sóis de vendedores, bandeiras ao vento...",
-                src: null,
+                src: "camiseta 2012 edgar SO FRENTE.png",
                 firstColor: "#009DFF",
                 secondColor: "#00BEB5",
                 link: '/2012/camiseta'
@@ -144,16 +144,16 @@ export const constantsCardsData = [
 
             {
                 title: 'Desfile',
-                description: "Em construção!",
-                src: null,
+                description: "O desfile realizado em 2012 destaca-se por sua reverência às tradições carnavalescas iniciadas pelo bloco, por presenças ilustres e por sua contribuição para a economia carioca - fatores esses que colaboraram para a vitória do Suvaco do Cristo no prêmio Serpentina de Ouro na categoria de figurinos.",
+                src: "330969_2616015796013_1745110318_o(1).jpg",
                 firstColor: "#E300C5",
                 secondColor: "#A700C8",
                 link: '/2012/desfile'
             },
             {
                 title: 'Suvaco na mídia',
-                description: "Em construção!",
-                src: null,
+                description: "Em 2012 o bloco foi assunto com frequência. E dessa vez, sempre de maneira positiva! As fantasias do bloco chamaram a atenção da imprensa… levando inclusive à obtenção do prêmio Serpentina de Ouro no quesito fantasias...",
+                src: "2012 Globo Anselmo gente Boa.jpg",
                 firstColor: "#DBBB00",
                 secondColor: "#FF9A27",
                 link: '/2012/midia'
