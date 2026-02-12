cc98996c9b421dd0bfb5ecdde1ddc17f0383af16
Carlos Chagas <139183141+carloschgas@users.noreply.github.com>
2026-02-12 13:38:18 -0300

    feat: constants projeto museu (equipe + textos página)

---
+++ src/constants/ProjetoMuseu/equipe.data.js
@@ -0,0 +1,72 @@
+export const equipeData = {
+    coordenacao: {
+        title: "Coordenação",
+        membros: ["anamaria"]
+    },
+
+    ufrj: {
+        title: "Membros da UFRJ",
+        membros: [
+            "claudiaGoes",
+            "heleniseMonteiro",
+            "hugoMusso",
+            "pauloMann",
+            "tiagoMatias",
+            "vanessaDupheim"
+        ]
+    },
+
+    parceiros: {
+        title: "Parceiros",
+        membros: ["carolineDeharbe", "joaoAvelleira"]
+    },
+
+    alunos: {
+        title: "Alunos da UFRJ",
+        membros: [
+            "carlosChagas",
+            "danielNocito",
+            "daviSouza",
+            "guilhermeCappeli",
+            "juliaVilela",
+            "lucasTanaka",
+            "maluOmena",
+            "riquelmeFreitas",
+            "vitorVicente",
+            "mariaLuisaCostaMata",
+            "rafaelOrioli",
+            "yuriAltomare",
+            "igorQueiroz",
+            "josephMeir",
+            "juliaLopes",
+            "leonardoFastovsky",
+            "leonardoFonseca",
+            "lucasDotto",
+            "lucasPinheiro",
+            "anaLuiza",
+            "bernardoAfonso",
+            "brunoCarvalho",
+            "fernandoJose",
+            "gabrielSantos",
+
+        ]
+    },
+
+    exMembros: {
+        title: "Ex-Membros",
+        membros: [
+            "annaCristina",
+            "arturAmaral",
+            "gabriellyFerreira",
+            "lauraSoares",
+            "mariaCarmo",
+            "igorAndrade",
+        ]
+    },
+    agradecimentos: {
+        title: "Agradecimentos",
+        membros: [
+          "isabelMartins"
+        ]
+    }
+}
