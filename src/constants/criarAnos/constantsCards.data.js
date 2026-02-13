38c9a832a3bf8fac719c908fa822fbcf1c37f59b
Carlos Chagas <139183141+carloschgas@users.noreply.github.com>
2026-02-12 23:50:31 -0300

    refact cards divinas

--- src/constants/criarAnos/constantsCards.data.js
+++ src/constants/criarAnos/constantsCards.data.js
@@ -2,6 +2,38 @@
 
 export const constantsCardsData = [
     {
+        ano: "divinas",
+        cards: [
+            {
+                title: 'Imprensa e Prêmios',
+                description: "A Divinas já recebeu prêmios como: o Economia Criativa (parte do programa Fomento a Iniciativas Empreendedoras e Inovadoras do Ministério da Cultura), na categoria “Novos Modelos de Empreendimentos e Negócios Criativos” e o Prêmio Fantasia do Serpentina de Ouro 2012.",
+                src: "2012 Serpentina de Ouro- Trofeu.jpg",
+                firstColor: "#00BE1D",
+                secondColor: "#00DE21",
+                link: '/ec'
+            },
+
+            {
+                title: 'Cursos e Oficinas',
+                description: "Além dos cursos regulares oferecidos na sede da ONG, a Divinas Axilas foi também responsável por oficinas de confecção de adereços e estandartes e de customização de camisetas , em parceria com o Centro de Referência do Artesanato Brasileiro (CRAB) e outros parceiros (Casa Bloco, Firjan, … ).",
+                src: "divinas3.jpg",
+                firstColor: "#009DFF",
+                secondColor: "#00BEB5",
+                link: '/ec'
+            },
+
+            {
+                title: 'Book',
+                description: "Em 2013-2014 foi criado um book para divulgar o projeto e os produtos da ONG. O book teve design desenvolvido por Lívia Martins e fotos de Ivanildo Carmo. Como modelos, amigos e parentes do pessoal envolvido com o projeto: Adriana Lessa, estilista do projeto na época, Lu, gerente, e muitos outros, que em breve creditaremos por aqui",
+                src: "bookdivinasfoto.png",
+                firstColor: "#E300C5",
+                secondColor: "#A700C8",
+                link: '/ec'
+            }
+
+        ]
+    },
+    {
         ano: "1986",
         cards: [
             {
