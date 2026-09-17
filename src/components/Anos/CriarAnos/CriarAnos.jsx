66a7aa99610231e1e215c1212fb486ae19672ddf
hugp-quip <quintelahugo13@gmail.com>
2026-09-17 20:39:56 -0300

    Páginas DivinasAxilas e CriarAnos movidas para novo componente.

--- src/components/Anos/CriarAnos/CriarAnos.jsx
+++ src/components/Anos/CriarAnos/CriarAnos.jsx
@@ -9,44 +9,47 @@ import group28 from "../../../assets/Group 28.svg"
 
 import { constantsAnosData } from "../../../constants/criarAnos/constantsAnos.data"
 import { constantsCardsData } from "../../../constants/criarAnos/constantsCards.data"
+import CriarPaginaComCards from "./CriarPaginaComCards"
 
 
 {/* como não estamos usando typescript, atenção pro ano que deve ser uma string*/ }
 
 export default function CriarAnos({ ano }) {
+  if (typeof ano !== 'string' || isNaN(ano) || !isFinite(ano)) 
+    console.error("O valor do ano passado para criar a página do ano não é uma string contendo apenas o número.");
 
-  const data = constantsAnosData.find(item => item.ano === ano)
-  const cardsData = constantsCardsData.find(item => item.ano === ano)
+  return(
+     <CriarPaginaComCards identificador={ano}/>
+  );
+  // const data = constantsAnosData.find(item => item.ano === ano)
+  // const cardsData = constantsCardsData.find(item => item.ano === ano)
 
-  if (!data || !cardsData) return null
+  // if (!data || !cardsData) return null
 
 
-  return (
+  // return (
 
-    <>
+  //   <>
 
-      {/*como estamos sem banco de dados, falta criar a requisicao pra
-      pegar as fotos respectivas de cada ano e passar pro <CriarCabecalhosAnos>
+  //     {/*como estamos sem banco de dados, falta criar a requisicao pra
+  //     pegar as fotos respectivas de cada ano e passar pro <CriarCabecalhosAnos>
       
       
       
-      A mesma coisa se aplica aos cards.
+  //     A mesma coisa se aplica aos cards.
       
-      */}
-      <CriarCabecalhoAnos data={data} />
+  //     */}
+  //     <CriarCabecalhoAnos data={data} />
 
 
-      <div className={styles.sectionCards}>
+  //     <div className={styles.sectionCards}>
 
-        <img src={group28} className={styles.group28Gradiente} alt="divisor ondulado" />
+  //       <img src={group28} className={styles.group28Gradiente} alt="divisor ondulado" />
 
-        <CriarCardsAnos ano = {ano} cardsData={cardsData.cards}> </CriarCardsAnos>
+  //       <CriarCardsAnos ano = {ano} cardsData={cardsData.cards}> </CriarCardsAnos>
 
-      </div>
+  //     </div>
     
-    </>
-
-
-
-  )
+  //   </>
+  //)
 }
--- src/components/DivinasAxilas/DivinasAxilas.jsx
+++ src/components/DivinasAxilas/DivinasAxilas.jsx
@@ -11,90 +11,94 @@ import divinas1 from "/divinas/divinas1.jpg"
 import divinas2 from "/divinas/divinas5.jpg"
 import divinas3 from "/divinas/divinas6.jpg"
 import divinas4 from "/divinas/divinas7.jpg"
+import CriarPaginaComCards from "../Anos/CriarAnos/CriarPaginaComCards";
 
 export default function DivinasAxilas() {
     
-    const cardData = constantsCardsData.find(item => item.ano === "divinas")
-    
+    // const cardData = constantsCardsData.find(item => item.ano === "divinas")
     return (
-        <>
-            <div className={styles.container}>
+        <CriarPaginaComCards identificador={"divinas"}/>
+    );
+    //
+    // return (
+    //     <>
+    //         <div className={styles.container}>
 
-                <div className={styles.cabecalho}>
-                    <div className={styles.title}>
-                        <h2 className={styles.blueTitles}>Divinas Axilas</h2>
-                    </div>
-
-
-                    <section >
-
-                        <img className={styles.imgDivinas} src={divinasImg} alt="" />
-                        <p className={styles.sectionTexts}>Fundada em 2008, a Divinas Axilas é uma ONG criada pelos integrantes do Suvaco do Cristo com o objetivo de capacitar, gerar renda e preservar a cultura popular.
-                            Utilizando como atividades o fornecimento de  cursos gratuitos semestrais de corte, modelagem e costura e oficinas para mulheres e homens no qual eram confeccionados fantasias, adereços, estandartes de carnaval e customização de camisetas e de carnaval durante todo o ano.
-                        </p>
-
-                        <p className={styles.sectionTexts}>
-                            As integrantes eram inicialmente  residentes da comunidade do Santa Marta, sendo aberto posteriormente para todas as comunidades e recebiam da ONG kits de material gratuitamente durante o curso.</p>
-
-                        <p className={styles.sectionTexts}>
-
-                            A Divinas já recebeu prêmios como: o Economia Criativa (parte do programa Fomento a Iniciativas Empreendedoras e Inovadoras do Ministério da Cultura), na categoria “Novos Modelos de Empreendimentos e Negócios Criativos” e o Prêmio Fantasia do Serpentina de Ouro 2012.
-                        </p>
+    //             <div className={styles.cabecalho}>
+    //                 <div className={styles.title}>
+    //                     <h2 className={styles.blueTitles}>Divinas Axilas</h2>
+    //                 </div>
 
 
-                    </section>
+    //                 <section >
+
+    //                     <img className={styles.imgDivinas} src={divinasImg} alt="" />
+    //                     <p className={styles.sectionTexts}>Fundada em 2008, a Divinas Axilas é uma ONG criada pelos integrantes do Suvaco do Cristo com o objetivo de capacitar, gerar renda e preservar a cultura popular.
+    //                         Utilizando como atividades o fornecimento de  cursos gratuitos semestrais de corte, modelagem e costura e oficinas para mulheres e homens no qual eram confeccionados fantasias, adereços, estandartes de carnaval e customização de camisetas e de carnaval durante todo o ano.
+    //                     </p>
+
+    //                     <p className={styles.sectionTexts}>
+    //                         As integrantes eram inicialmente  residentes da comunidade do Santa Marta, sendo aberto posteriormente para todas as comunidades e recebiam da ONG kits de material gratuitamente durante o curso.</p>
+
+    //                     <p className={styles.sectionTexts}>
+
+    //                         A Divinas já recebeu prêmios como: o Economia Criativa (parte do programa Fomento a Iniciativas Empreendedoras e Inovadoras do Ministério da Cultura), na categoria “Novos Modelos de Empreendimentos e Negócios Criativos” e o Prêmio Fantasia do Serpentina de Ouro 2012.
+    //                     </p>
+
+
+    //                 </section>
 
                    
-                    <img src={group28} className={styles.group28Gradiente} alt="divisor ondulado" />
+    //                 <img src={group28} className={styles.group28Gradiente} alt="divisor ondulado" />
 
-                </div>
+    //             </div>
 
-                <div className={styles.areaVerde}>
-
-
-
-                    <div className={styles.cardContainer}>
-                        <CriarCardsAnos ano ={"divinas"} cardsData={cardData.cards}/>
-                    </div>
-
-                    <section className={styles.fotosSection}>
-
-                        <h3 className={styles.sectionTitle}>Fotos</h3>
-
-                        <div className={styles.gridVisual}>
-                            <img src={divinas3} className={styles.imgBox} alt="" />
-                            <img src={divinas2} className={styles.imgBox} alt="" />
-                            <img src={divinas1} className={styles.imgBox} alt="" />
-                            <img src={divinas4} className={styles.imgBox} alt="" />
-                        </div>
-                    </section>
+    //             <div className={styles.areaVerde}>
 
 
-                    {/*  <section>
-                        <h3 className={styles.sectionTitle} >Links</h3>
-                        <div className={styles.linksGradiente}/>
-                        <div className= {styles.linksDiv}>
-                             <a href="https://www.instagram.com/suvacodocristooficial" target="_blank" rel="noopener noreferrer">
-                    <i className="ri-instagram-line"></i><span> Instagram</span>
-                </a>
-                <a href="https://www.youtube.com/@blocosuvacodecristorj" target="_blank" rel="noopener noreferrer">
-                    <i className="ri-youtube-line"></i><span> Youtube</span>
-                </a>
-                <a href="https://www.facebook.com/suvaco.docristo" target="_blank" rel="noopener noreferrer">
-                    <i className="ri-facebook-box-line"></i><span> Facebook</span>
-                    </a>
-                        </div>
+
+    //                 <div className={styles.cardContainer}>
+    //                     <CriarCardsAnos ano ={"divinas"} cardsData={cardData.cards}/>
+    //                 </div>
+
+    //                 <section className={styles.fotosSection}>
+
+    //                     <h3 className={styles.sectionTitle}>Fotos</h3>
+
+    //                     <div className={styles.gridVisual}>
+    //                         <img src={divinas3} className={styles.imgBox} alt="" />
+    //                         <img src={divinas2} className={styles.imgBox} alt="" />
+    //                         <img src={divinas1} className={styles.imgBox} alt="" />
+    //                         <img src={divinas4} className={styles.imgBox} alt="" />
+    //                     </div>
+    //                 </section>
+
+
+    //                 {/*  <section>
+    //                     <h3 className={styles.sectionTitle} >Links</h3>
+    //                     <div className={styles.linksGradiente}/>
+    //                     <div className= {styles.linksDiv}>
+    //                          <a href="https://www.instagram.com/suvacodocristooficial" target="_blank" rel="noopener noreferrer">
+    //                 <i className="ri-instagram-line"></i><span> Instagram</span>
+    //             </a>
+    //             <a href="https://www.youtube.com/@blocosuvacodecristorj" target="_blank" rel="noopener noreferrer">
+    //                 <i className="ri-youtube-line"></i><span> Youtube</span>
+    //             </a>
+    //             <a href="https://www.facebook.com/suvaco.docristo" target="_blank" rel="noopener noreferrer">
+    //                 <i className="ri-facebook-box-line"></i><span> Facebook</span>
+    //                 </a>
+    //                     </div>
                         
-                    </section> */}
+    //                 </section> */}
 
-                    <BtnVoltar></BtnVoltar>
-                    <div className={styles.padding}></div>
+    //                 <BtnVoltar></BtnVoltar>
+    //                 <div className={styles.padding}></div>
 
-                </div>
+    //             </div>
 
 
-            </div>
-        </>
-    );
+    //         </div>
+    //     </>
+    // );
 }
 
